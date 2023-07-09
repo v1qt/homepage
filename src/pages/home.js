92c67198684ee2eb19ff103171b90f0206dbff71
v1qt <woodoowanya@gmail.com>
2023-07-09 17:54:39 +0200

    fift commit

--- src/pages/home.js
+++ src/pages/home.js
@@ -31,7 +31,7 @@ function Copyright() {
     );
   }
   
-  const cards = [1, 2, 3, 4, 5, 6, 7];
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   
   // TODO remove, this demo shouldn't need to reset the theme.
   const defaultTheme = createTheme();
